4baca6c4cc3e97490552fb1d6eb4661481ad810a
Gabe Rodriguez <grodriguez@grodriguez-macbookpro.roam.corp.google.com>
2017-11-14 09:55:11 +0100

    added server compression package

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(compression());
 
 app.use(express.static(path.resolve(__dirname, './react-ui/build')));
 
