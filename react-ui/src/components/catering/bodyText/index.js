f8762fdf4e48dacc25d61688ce1c400ea8d322a6
Gabe Rodriguez <grod220@gmail.com>
2017-11-29 09:30:18 +0100

    body text done

--- react-ui/src/components/catering/bodyText/index.js
+++ react-ui/src/components/catering/bodyText/index.js
@@ -1,9 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import { media } from "../../../sharedUtilities/media";
+
+import Highlight from "../../shared/highlight/";
+
+const Container = styled.div`
+  width: 75vw;
+  margin: 4rem auto;
+
+  li {
+    margin-bottom: 2rem;
+  }
+
+  ${media.tablet`
+    width: 90vw`}
+
+  ${media.phone`
+    width: 90vw`}
+`
 
 const BodyText = () => (
-  <div>
-      BODY TEXT GOES HERE
-  </div>
+  <Container>
+    Let us take away your pressure! The Meatball Stoppe can be the missing
+    ingredient for your event.
+    <ul>
+      <li>
+        <Highlight i>Corporate or social catering</Highlight> including weddings, cocktail receptions,
+        dinner parties, holiday parties.... and more. You name it, we do it.
+      </li>
+      <li>
+      <Highlight i>Private Cooking Classes</Highlight> used for Corporate Team Building, Employee and
+        Client Appreciation and many other celebrations.
+      </li>
+      <li><Highlight i>Premier Dining Experience</Highlight> with Chef Isabella in your home.</li>
+    </ul>
+  </Container>
 );
 
 export default BodyText;
--- react-ui/src/components/shared/highlight/index.js
+++ react-ui/src/components/shared/highlight/index.js
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export default styled.span`
   color: #902e2d;
+  font-style: ${props => (props.i ? "italic" : props.b ? "bold" : "normal")};
 `;
