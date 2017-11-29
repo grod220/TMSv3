dbd16d201c82867081ef22f640b4018b07077d5f
Gabe Rodriguez <grod220@gmail.com>
2017-11-29 22:09:25 +0100

    dining room completed

--- react-ui/src/components/aboutUs/diningRoom/index.js
+++ react-ui/src/components/aboutUs/diningRoom/index.js
@@ -2,10 +2,43 @@ import React from "react";
 import styled from "styled-components";
 import { media } from "../../../sharedUtilities/media";
 
+import DiningRoomPic from "./diningRoom.jpg";
+
+const Container = styled.div`
+  display: flex;
+
+  ${media.phone`
+    flex-direction: column`}
+`;
+
+const LeftPic = styled.div`
+  flex: 1;
+  background: url(${DiningRoomPic});
+  background-size: cover;
+  background-position: 50%;
+  height: 25rem;
+
+`;
+
+const RightTextBlock = styled.div`
+  color: white;
+  padding: 4rem;
+  flex: 1;
+  background-color: #bd977a;
+
+  ${media.phone`
+    padding: 2rem;`}
+`;
+
 const DiningRoom = () => (
-  <div>
-      DINING ROOM CONTENT HERE
-  </div>
+  <Container>
+    <LeftPic />
+    <RightTextBlock>
+      <strong>NEW!</strong> Private dining room available for rent for meetings,
+      parties or engagements. Wi-Fi and HDMI TV. Call Isabella at 407-267-6033
+      for details.
+    </RightTextBlock>
+  </Container>
 );
 
 export default DiningRoom;
