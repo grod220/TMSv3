01ca94fe45d9670dfd3d662b40a05fc435757f51
Gabe Rodriguez <grod220@gmail.com>
2017-11-30 19:00:39 +0100

    images renamed and flexbox container setup

--- react-ui/src/components/media/tripleD/index.js
+++ react-ui/src/components/media/tripleD/index.js
@@ -1,11 +1,45 @@
 import React from "react";
-import styled from "styled-components"
-import {media} from "../../../sharedUtilities/media"
+import styled from "styled-components";
+import { media } from "../../../sharedUtilities/media";
+
+import GuyFieriRays from "./images/guyFieriRays.jpg";
+import dddLogo from "./images/dddLogo.png";
+import groupPic from "./images/groupPic.jpg";
+
+const Container = styled.div`
+  background: url(${GuyFieriRays}) no-repeat;
+  background-size: 100%;
+  background-position: top;
+  height: 45rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TopTwo = styled.div`
+    display: flex;
+    width: 80vw;
+`
+
+const TripleDHero = styled.div``;
+const SupplementalPics = styled.div``;
+const Quote = styled.p``;
 
 const TripleD = () => (
-  <div>
-      TRIPLE D SECTION
-  </div>
+  <Container>
+    <TopTwo>
+      <TripleDHero>
+        <img src={dddLogo} width="100"/>
+        <img src={groupPic} width="100" />
+      </TripleDHero>
+      <SupplementalPics>Stuff to come!</SupplementalPics>
+    </TopTwo>
+    <Quote>
+      “It's really good. It's a cross between light and dense. Delicious... They
+      are super tender. Out of the two meatballs, I like the vegetarian the
+      most! This is something to write home about. Very nice.” -Guy Fieri
+    </Quote>
+  </Container>
 );
 
 export default TripleD;
