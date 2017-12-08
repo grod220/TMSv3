48b041b62bd8b7c26e45fdb12161c1b1010743bc
Gabe Rodriguez <gabe@spotify.com>
2017-12-08 23:39:15 +0100

    google analytics installed

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import Navigation from "./components/shared/navigation/";
 import Homepage from "./components/homepage";
 import Menu from "./components/menu";
@@ -6,22 +7,35 @@ import Catering from "./components/catering";
 import AboutUs from "./components/aboutUs";
 import Media from "./components/media";
 import Delivery from "./components/delivery";
+import createHistory from "history/createBrowserHistory";
 
+import { Router, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-55491347-1");
+
+const history = createHistory();
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 class App extends Component {
+  componentWillMount() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <div>
           <Navigation />
           <Route exact path="/" component={Homepage} />
-          <Route path="/menu" component={Menu}/>
-          <Route path="/catering" component={Catering}/>
-          <Route path="/aboutus" component={AboutUs}/>
-          <Route path="/media" component={Media}/>
-          <Route path="/delivery" component={Delivery}/>
+          <Route path="/menu" component={Menu} />
+          <Route path="/catering" component={Catering} />
+          <Route path="/aboutus" component={AboutUs} />
+          <Route path="/media" component={Media} />
+          <Route path="/delivery" component={Delivery} />
         </div>
       </Router>
     );
