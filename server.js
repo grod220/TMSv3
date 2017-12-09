5189740138e8d4e496c19fc73d1ec920ab6ab84c
Gabe Rodriguez <gabe@spotify.com>
2017-12-09 13:00:01 +0100

    made exemption for Localhost:5000 so I can continue work in local environment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const compression = require("compression");
 const bodyParser = require("body-parser");
-const enforce = require("express-sslify");
+const enforce = require("./enforceSSL");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,7 +11,7 @@ app.use(compression());
 require("dotenv").config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-enforce.HTTPS({ trustProtoHeader: true });
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(express.static(path.resolve(__dirname, "./react-ui/build")));
 
