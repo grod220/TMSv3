8c2c9c5ca891dc6c47ed4d19ab7aac7f2e1d439a
Gabe Rodriguez <gabe@spotify.com>
2017-12-09 13:21:23 +0100

    implemented lazy loading for homepage

--- react-ui/src/components/homepage/index.js
+++ react-ui/src/components/homepage/index.js
@@ -5,20 +5,34 @@ import OrderBar from "./orderBar/";
 import SocialBar from "./socialBar/";
 import HomeMenu from "./homeMenu/";
 import TestimonialTaster from "./testimonialTaster/";
-import Map from "./map/"
-import Passion from "./passion/"
-import Footer from "./footer/"
+import Map from "./map/";
+import Passion from "./passion/";
+import Footer from "./footer/";
+
+import LazyLoad from "react-lazyload";
 
 const Homepage = () => (
   <div>
     <TallHero />
     <OrderBar />
-    <SocialBar />
-    <HomeMenu />
-    <TestimonialTaster />
-    <Map />
-    <Passion />
-    <Footer />
+    <LazyLoad height={350}>
+      <SocialBar />
+    </LazyLoad>
+    <LazyLoad height={400}>
+      <HomeMenu />
+    </LazyLoad>
+    <LazyLoad height={350}>
+      <TestimonialTaster />
+    </LazyLoad>
+    <LazyLoad height={450}>
+      <Map />
+    </LazyLoad>
+    <LazyLoad height={400}>
+      <Passion />
+    </LazyLoad>
+    <LazyLoad height={160}>
+      <Footer />
+    </LazyLoad>
   </div>
 );
 
